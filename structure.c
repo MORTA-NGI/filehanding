6d9927656cfbed253fb67d428a75276a0b5f78de
morta-ngi <mortdaa@ngi.ac.za>
2024-08-13 11:15:15 +0200

    new update

--- structure.c
+++ structure.c
@@ -5,27 +5,20 @@ struct Student {
     char name[50];
     float grade;
 };
-
 int main() {
     int n;
-
     // Ask the user for the number of students
     printf("Enter the number of students: ");
     scanf("%d", &n);
-
     // Create an array of structures
     struct Student students[n];
-
     // Input student information
     for(int i = 0; i < n; i++) {
         printf("\nEnter details for student %d:\n", i + 1);
-
         printf("Enter ID: ");
         scanf("%d", &students[i].id);
-
         printf("Enter name: ");
         scanf("%s", students[i].name);
-
         printf("Enter grade: ");
         scanf("%f", &students[i].grade);
     }
